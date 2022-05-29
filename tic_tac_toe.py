916d91b4942b3cb6499a926ee63df0f0c1a8462f
Hazem-Gamall <hazemgamalelgendy@gmail.com>
2022-05-30 00:47:44 +0200

    Revert "removed unnecesarry minimax function"
    This reverts commit ba61a8a66f11b2000a5d265d171b761f4eff69ff.

--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -42,6 +42,12 @@ class TicTacToe:
         return value
 
 
+    def minimax(self): 
+        value = self.min_val()
+        return value
+
+
+
     def best_move(self):
         print("Computer's move...")
         allowed_moves = self.board.actions(self.computer)
@@ -49,7 +55,7 @@ class TicTacToe:
         best_move = ()
         for move in allowed_moves:
             self.board.board[move[0]][move[1]] = self.computer
-            score = self.min_val()
+            score = self.minimax()
             self.board.board[move[0]][move[1]] = 0
             # print(move, score)
             if score > best_score:
